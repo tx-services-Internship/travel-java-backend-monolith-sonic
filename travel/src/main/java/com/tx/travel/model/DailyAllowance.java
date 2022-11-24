f59dac378c7e4354898e9a0ddd9955bce1790e89
Milena <milena.petrovic@tx.group>
2022-11-24 12:42:18 +0100

    CRUD Daily Allowances - in progress

--- travel/src/main/java/com/tx/travel/model/DailyAllowance.java
+++ travel/src/main/java/com/tx/travel/model/DailyAllowance.java
@@ -5,11 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 @Builder
